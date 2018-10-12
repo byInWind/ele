538b07541d0f887ea861d1476e2a362bc4c36fd4
lihao <lihao.cuapp.me>
2018-10-12 18:02:54 +0800

    完善loading页

--- src/components/BaseLoading.vue
+++ src/components/BaseLoading.vue
@@ -1,25 +1,80 @@
 <template>
-    <div>
-        loading
+    <div class="loading_container">
+        <div class="load_img" :style="{backgroundPositionY: -(positionY%7)*2.5 + 'rem'}">
+        </div>
+        <img class="load_ellipse" src="../../public/a.png" alt="">
     </div>
-
 </template>
 
 <script>
     export default {
-        name: 'BaseLoading'
-
+        name: 'BaseLoading',
+        data() {
+            return {
+                positionY: 0,
+                timer: null,
+            }
+        },
+        mounted() {
+            this.timer = setInterval(() => {
+                this.positionY++;
+            }, 600)
+        },
+        beforeDestroy() {
+            clearInterval(this.timer);
+        }
     }
 </script>
 
-<style scoped lang="scss">
-  div{
-      top: 200px;
-      text-align: center;
-      position: fixed;
-      z-index: 99999;
-      color: #3190e8;
-      width: 100%;
-      font-size: 50px;
-  }
+<style lang="scss" scoped>
+    @keyframes load {
+        0% {
+            transform: translateY(0px);
+        }
+        50% {
+            transform: translateY(-50px);
+        }
+        100% {
+            transform: translateY(0px);
+        }
+    }
+
+    @keyframes ellipse {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(0.3);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+
+    .loading_container {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        width: 75px;
+        height: 75px;
+        z-index: 9;
+        transform: translate(-50%, -50%);
+    }
+
+    .load_img {
+        background: url("../../public/icon_loading.png") no-repeat 0 0;
+        background-size: 75px auto;
+        transform: translateY(0px);
+        animation: load .6s infinite ease-in-out;
+        position: relative;
+        width: 75px;
+        height: 75px;
+    }
+
+    .load_ellipse {
+        position: absolute;
+        top: 81px;
+        left: -17px;
+        animation: ellipse .6s infinite ease-in-out;
+    }
 </style>
